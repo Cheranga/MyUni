6fb5c0b3f3ed7bfc8579bac64d4d87d5bb47ea9b
Cheranga Hatangala <cheranga@gmail.com>
2015-09-01 21:52:58 +1000

    Bootstrap data table changes are correctly incorporated. Next step is to implement the edit, details and delete functionality.

---
+++ MyUni.Web/Scripts/App/Students/index.js
@@ -0,0 +1,99 @@
+﻿var app = app || {};
+
+app.StudentsViewModel = function() {
+    var self = {};
+
+    //
+    // Initialize the grid
+    //
+    self.studentsView = $("#tblStudentsView").DataTable({
+        columns: [
+            { data: "FirstName" },
+            { data: "LastName" },
+            {
+                data: "EnrolledDate",
+                render: function(dateObj) {
+                    //
+                    // To format the date properly
+                    //  http://stackoverflow.com/questions/726334/asp-net-mvc-jsonresult-date-format
+                    //
+                    var oDate = new Date(parseInt(dateObj.substr(6))); //new Date(dateObj.aData[0]);
+                    var result = oDate.getDate() + "/" + (oDate.getMonth() + 1) + "/" + oDate.getFullYear();
+                    return "<span>" + result + "</span>";
+                }
+            },
+            {
+                data: null,
+                sortable: false,
+                render: function(dataObj) {
+                    return $('#tmpActions').html();
+                }
+            }
+        ],
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "autoWidth": true,
+        //"searchable":false,
+        "processing": true,
+        "serverSide": true,
+        "ajax": $("#tblStudentsView").data('url')
+    });
+
+    //
+    // Search function
+    //
+    self.search = function() {
+        var url = $('#tblStudentsView').data('url') + "?search=" + $('#search').val();
+        self.studentsView.ajax.url(url).load();
+    };
+
+    //
+    // Initialize the edit buttons
+    //
+    self.initCommandButtons = function (e) {
+
+        //
+        // Upon selection the grid row will be marked as selected
+        //
+        $('#tblStudentsView tbody').on('click', 'tr', function () {
+
+            $('#tblStudentsView tbody tr').removeClass('warning');
+            $(this).addClass('warning');
+        });
+
+        //
+        // Edit button
+        //
+        $('#tblStudentsView').on('click', 'tbody tr .editBtn', function (e) {
+
+            var tr = $(this).closest('tr');
+            var data = self.studentsView.row(tr).data();
+            console.log(data);
+        });
+
+    };
+
+    //
+    // Set the search functionality to happen upon ENTER key press, and on the search button click
+    //
+    self.initSearch = function () {
+
+        $('#search').on('keypress', function (e) {
+            var key = e.which;
+
+            if (key === 13) {
+                self.search();
+            }
+        });
+    };
+
+    self.init = function() {
+        self.initSearch();
+        self.initCommandButtons();
+    };
+
+    return {
+        init:self.init
+    }
+}
